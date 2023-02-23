35e219c72f6bda8bd02902459adea9b808826ef6
Moppieee <troxisai@kgg-do.eu>
2023-02-23 18:37:24 +0100

    Initial commit

---
+++ src/Lehrkraft.java
@@ -0,0 +1,16 @@
+public class Lehrkraft {
+    private String besoldungsstufe;
+
+
+    public Lehrkraft(){
+         Lehrkraft l1 = new Lehrkraft();
+    }
+
+    public String getBesoldungsstufe() {
+        return besoldungsstufe;
+    }
+
+    public void setBesoldungsstufe(String besoldungsstufe) {
+        this.besoldungsstufe = besoldungsstufe;
+    }
+}
---
+++ src/Schulkraft.java
@@ -0,0 +1,15 @@
+public class Schulkraft {
+    private int beliebtheit;
+
+    public Schulkraft(){
+    
+    }
+
+    public int getBeliebtheit() {
+        return beliebtheit;
+    }
+
+    public void setBeliebtheit(int beliebtheit) {
+        this.beliebtheit = beliebtheit;
+    }
+}
